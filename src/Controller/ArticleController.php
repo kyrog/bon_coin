3f88a84962580ec845fc93afdc53eef223b78b64
kyrog <franck.salhi@epitech.eu>
2022-02-01 18:07:53 +0100

    display articles in article page

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,19 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
+
+    private $articleRepository;
+    private $em;
+
+    public function __construct(ArticleRepository $articleRepository, EntityManagerInterface $em) {
+        $this->articleRepository = $articleRepository;
+        $this->em = $em;
+    }
     /**
      * @Route("/article", name="article")
      */
     public function index(): Response
     {
+        $articles = $this->articleRepository->findAll();
         return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+            'articles' => $articles,
         ]);
     }
 }
